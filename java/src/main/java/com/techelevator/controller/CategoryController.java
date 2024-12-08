447be69b7d3abfb8b82870f76a43b7aee4c84328
Froboz67 <kengel.ipa@gmail.com>
2024-12-08 08:39:04 -0500

    ready to create category branch

--- java/src/main/java/com/techelevator/controller/CategoryController.java
+++ java/src/main/java/com/techelevator/controller/CategoryController.java
@@ -2,9 +2,11 @@ package com.techelevator.controller;
 
 
 import com.techelevator.dao.ItemCategoryDao;
+import com.techelevator.model.ItemCategory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +16,9 @@ public class CategoryController {
 
 //    @Autowired
 //    private ItemCategoryDao itemCategoryDao;
+//
+//    @GetMapping("/categories")
+//    public ItemCategory getItemCategories
 
 
 
